b712ea75de6776e598e3d4677287e795379b36ce
Levente Schoenherr <levente.schoenherr@otto.de>
2023-04-22 11:07:14 +0200

    Initial transcription function works

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import torch.cuda
 
 
@@ -5,4 +7,5 @@ class Config:
     def __init__(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.audio_path = "../data/effibriest_01_fontane_64kb.mp3"
+        self.local_audio_path = tempfile.mkdtemp()
         self.whisper_model = "tiny"
--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import json
+import os.path
+
+import aiofiles
 import auditok
+import uvicorn
+
+from typing import Annotated, Union
 
 import whisper_timestamped as whisper
+from fastapi import FastAPI, File, UploadFile
 
 from src.config import Config
+
+app = FastAPI()
 
 
 def whisper_transcribe(config):
@@ -12,14 +21,35 @@ def whisper_transcribe(config):
     return whisper.transcribe(whisper_model, audio)
 
 
-config = Config()
-result = whisper_transcribe(config)
+def main():
+    config = Config()
+    result = whisper_transcribe(config)
 
-with open("whisper_result.json", "w") as out_file:
-    out_file.write(json.dumps(result, indent=2, ensure_ascii=False))
+    with open("whisper_result.json", "w") as out_file:
+        out_file.write(json.dumps(result, indent=2, ensure_ascii=False))
 
-region = auditok.load(config.audio_path)  # returns an AudioRegion object
-regions = region.split_and_plot(drop_trailing_silence=True)  # or just region.splitp()
+    region = auditok.load(config.audio_path)  # returns an AudioRegion object
+    regions = region.split_and_plot(drop_trailing_silence=True)  # or just region.splitp()
 
 
-print("a")
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/transcribe")
+async def create_upload_file(file: Union[UploadFile, None] = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        #  save uploaded file to tmp
+        async with aiofiles.open(os.path.join(config.local_audio_path, file.filename), 'wb') as out_file:
+            content = await file.read()  # async read
+            await out_file.write(content)  # async write
+        return whisper_transcribe(config)
+
+
+if __name__ == '__main__':
+    config = Config()
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
